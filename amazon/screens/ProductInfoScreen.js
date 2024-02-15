b60b9cc045f2ad47297ee0b6ef951ee337a0b217
ViniTex231 <viniteixeira2828@gmail.com>
2024-02-15 16:54:44 -0300

    feat: product descriptions and delivery info

--- amazon/screens/ProductInfoScreen.js
+++ amazon/screens/ProductInfoScreen.js
@@ -1,11 +1,71 @@
-import { StyleSheet, Text, View, ScrollView } from 'react-native'
+import { StyleSheet, Text, View, ScrollView, Pressable, TextInput, ImageBackground, Dimensions } from 'react-native'
 import React from 'react'
+import { AntDesign, Feather, MaterialCommunityIcons, Ionicons } from '@expo/vector-icons'
+import { useNavigation, useRoute } from '@react-navigation/native'
 
 const ProductInfoScreen = () => {
+    const route = useRoute()
+    const { width } = Dimensions.get('window')
+    const navigation = useNavigation()
+    const height = (width * 100) / 100
   return (
-    <ScrollView style showsVerticalScrollIndicator={false}>
-      <Text>ProductInfoScreen</Text>
+    <ScrollView style={{marginTop: 55, flex: 1, backgroundColor: 'white'}} showsVerticalScrollIndicator={false}>
+        <View style={{backgroundColor: '#00CED1', padding: 10, flexDirection: 'row', alignItems: 'center'}}>
+            <Pressable style={{flexDirection: 'row', alignItems: 'center', marginHorizontal: 7, gap: 10, backgroundColor: 'white', borderRadius: 3, height: 38, flex: 1}}>
+                <AntDesign style={{paddingLeft: 10}} name="search1" size={22} color="black" />
+                <TextInput placeholder='Procurar' />
+            </Pressable>
+
+            <Feather name="mic" size={24} color="black" />
+        </View>
+
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            {route.params.carouselImages.map((item, index) => (
+                <ImageBackground style={{width, height, marginTop: 25, resizeMode: 'contain'}} source={{uri:item}} key={index}>
+                    <View style={{padding: 20, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
+                        <View style={{width: 40, height: 40, borderRadius: 20, backgroundColor: '#C60C30', justifyContent: 'center', alignItems: 'center', flexDirection: 'row'}}>
+                            <Text style={{color: 'white', textAlign: 'center', fontWeight: '600', fontSize: 12}}>28% off</Text>
+                        </View>
+
+                        <View style={{width: 40, height: 40, borderRadius: 20, backgroundColor: '#E0E0E0', justifyContent: 'center', alignItems: 'center', flexDirection: 'row'}}>
+                            <MaterialCommunityIcons name='share-variant' size={24} />
+                        </View>
+                    </View>
+
+                    <View style={{width: 40, height: 40, borderRadius: 20, backgroundColor: '#E0E0E0', justifyContent: 'center', alignItems: 'center', flexDirection: 'row', marginTop: 'auto', marginLeft: 20, marginBottom: 20}}>
+                        <AntDesign name='hearto' size={24} color='black' />
+                    </View>
+                </ImageBackground>
+            ))}
+        </ScrollView>
+
+        <View style={{padding: 10}}>
+            <Text style={{fontSize: 15, fontWeight: '500'}}>{route?.params?.title}</Text>
+            <Text style={{fontSize: 18, fontWeight: '600', marginTop: 6}}>R${route?.params?.price}</Text>
+        </View>
+        <Text style={{height: 1, borderColor: '#D0D0D0', borderWidth: 1}} />
+        <View style={{flexDirection: 'row', alignItems: 'center', padding: 10}}>
+            <Text>Cor: </Text>
+            <Text style={{fontSize: 15, fontWeight: 'bold'}}>{route?.params?.color}</Text>
+        </View>
+
+        <View style={{flexDirection: 'row', alignItems: 'center', padding: 10}}>
+            <Text>Tamanho: </Text>
+            <Text style={{fontSize: 15, fontWeight: 'bold'}}>{route?.params?.size}</Text>
+        </View>
+        <Text style={{height: 1, borderColor: '#D0D0D0', borderWidth: 1}} />
+
+        <View style={{padding: 10}}>
+            <Text style={{fontSize: 15, fontWeight: 'bold', marginVertical: 5}}>Total: {route.params.price}</Text>
+            <Text style={{color: '#00CED1'}}>Frete GRÁTIS Comprando até 12H</Text>
+
+            <View style={{flexDirection: 'row', marginVertical: 5, alignItems: 'center', gap: 5}}>
+                <Ionicons name='location' size={24} color='black' />
+                <Text style={{fontSize: 15, fontWeight: '500'}}>Entregar para Vinícius - Rua A 98</Text>
+            </View>
+        </View>
     </ScrollView>
+
   )
 }
 
